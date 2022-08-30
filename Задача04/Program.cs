b564c2effffcab5abbe656cb5476b3b495cf766d
Kirill <kirilltcarev1999@gmail.com>
2022-08-30 14:39:04 +0300

    добавил задачу 4

---
+++ Задача04/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while (count <= num1)
+{
+    if(count %2 == 0)
+    Console.WriteLine(count);
+    count=count+1;
+}
